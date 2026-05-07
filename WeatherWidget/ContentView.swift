06a63bd2d5e7299f2eff0b4e0d2fe68cd1eaca31
Lan Le <anle0689@uni.sydney.edu.au>
2026-05-07 18:37:27 +1000

    Initial Commit

---
+++ WeatherWidget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherWidget
+//
+//  Created by Lan Le on 7/5/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherWidget/WeatherWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherWidgetApp.swift
+//  WeatherWidget
+//
+//  Created by Lan Le on 7/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
